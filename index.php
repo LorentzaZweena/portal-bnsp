942a2b68f4ef1218c8f3365a57751fe5159ad2ad
LorentzaZweena <ariva02zweena@gmail.com>
2025-01-21 15:18:21 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portal BNSP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <style>
+        .card {
+            transition: transform 0.3s ease;
+        }
+        
+        .card:hover {
+            transform: scale(1.1);
+            cursor: pointer;
+        }
+    </style>
+  </head>
+  <body>
+    <div class="container mt-5">
+        <h2 class="text-white">Ariva Zweena</h2>
+        <h2 class="text-white">XII RPL</h2>
+    </div>
+    <div class="container text-center mt-5">
+        <h1>PORTAL LATIHAN UKK</h1>
+    </div>
+    <div class="container align-items-center mt-5">
+    <div class="d-flex flex-row mb-3 justify-content-center">
+                    <div class="p-2">
+                    <div class="card text-center" style="width: 20rem;">
+        <img src="undraw_learning_2jue.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title text-center">Latihan UKK 1</h5>
+            <p class="card-text text-center">Studi kasus : Pendaftaran beasiswa</p>
+            <a href="../beasiswa/" target="_blank" class="btn btn-primary align-item-center">Hasil latihan</a>
+        </div>
+        </div>
+            </div>
+            <div class="p-2">
+                    <div class="card text-center" style="width: 20rem;">
+        <img src="undraw_learning_2jue.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title text-center">Latihan UKK 2</h5>
+            <p class="card-text text-center">Studi kasus : Penyewaan hotel</p>
+            <a href="../penyewaan-hotel/" target="_blank" class="btn btn-primary align-item-center">Hasil latihan</a>
+        </div>
+        </div>
+            </div>
+            <div class="p-2">
+                    <div class="card text-center" style="width: 20rem;">
+        <img src="undraw_learning_2jue.png" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title text-center">Latihan UKK 3</h5>
+            <p class="card-text text-center">Studi kasus : Pemesanan tiket wisata</p>
+            <a href="../pemesanan-tiket/" class="btn btn-primary" target="_blank">Hasil latihan</a>
+        </div>
+        </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
